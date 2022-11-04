887e03c412d15c8591a269fe75a18dfd71a4124e
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-04 14:57:58 +0100

    delete /oda/ header.php

--- App/@/inc/header.php
+++ App/@/inc/header.php
@@ -21,14 +21,14 @@ if (session_status() == PHP_SESSION_NONE) {
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container">
                 <img class="img-fluid" src="App/public/assets/image/logoFitnessCore.jpg">
-                <a class="navbar-brand" href="oda/">Fitness Core</a>
+                <a class="navbar-brand" href="App/public/src/index.php">Fitness Core</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarScroll">
                     <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/oda/">Accueil</a>
+                            <a class="nav-link active" aria-current="page" href="App/public/src/index.php">Accueil</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="App/public/src/a_propos.php">À propos</a>
