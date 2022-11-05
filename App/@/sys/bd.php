7245b5a02eae1f83c17c6028bf2a0b4f769cc91b
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-05 22:41:36 +0100

    test condition jawsdb dans bd.php

--- App/@/sys/bd.php
+++ App/@/sys/bd.php
@@ -3,11 +3,21 @@
 if(getenv('JAWSDB_URL') !== false){
     $url = getenv('JAWSDB_URL');
     $dbparts = parse_url($url);
-    
+
     $hostname = $dbparts['host'];
     $username = $dbparts['user'];
     $password = $dbparts['pass'];
-    $database = ltrim($dbparts['path'],'/');    
+    $database = ltrim($dbparts['path'],'/');
+    try {
+        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+        }
+    catch(PDOException $e)
+        {
+        echo "Connection failed: " . $e->getMessage();
+        }
 } else {
     $pdo = new PDO('mysql:dbname=oda;host=localhost', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
