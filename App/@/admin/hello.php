186659381112d6b50b0bbf35ef4e56ea63bc70fd
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-07 20:51:44 +0100

    test hello.php 2

--- App/@/admin/hello.php
+++ App/@/admin/hello.php
@@ -1,12 +1,4 @@
 <?php $pageName = 'Panel PDG';
+include '../../@/inc/header.php';
 
 require_once '../../@/sys/functions.php';
-require_once '../../@/sys/bd.php';
-require_once '../../@/sys/roles.php';
-require_once '../../@/sys/salles.php';
-
-$req = $pdo->prepare('SELECT id, username, surname, email, age, roles,confirmed_at,salle_id,user_secret FROM users ORDER BY roles DESC');
-$req->execute();
-$users = $req->fetchAll();
-
-include '../../@/inc/header.php';?>
