2a8d516468afc4078ce2f651d909ca77ec8a0a2a
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-07 21:05:50 +0100

    manque le html de hello.php

--- App/@/admin/hello.php
+++ App/@/admin/hello.php
@@ -7,7 +7,7 @@ if(!isset($_SESSION['auth'])){
     exit();
 }
   
-require_once '../../@/sys/functions.php';
+require_once '/App/@/sys/functions.php';
 require_once '/App/@/sys/bd.php';
 require_once '/App/@/sys/roles.php';
 require_once '/App/@/sys/salles.php';
@@ -20,14 +20,13 @@ include '../../@/inc/header.php';
 ?>
 
 <div class="w-100 p-5 my-6 m-auto bg-light table-responsive rounded-3">
-<h1>Bonjour, <?= $_SESSION['auth']->username ?></h1>
+<h1>Bonjour, <?php echo $_SESSION['auth']->username ?></h1>
 <div class="d-flex justify-content-between">
 <h4 class="mb-4">Voici la liste des employés :</h4>
 <a class="btn btn-success my-2" href="/App/@/admin/create.php">Créer un employé</a>
     </div>
 
     <table class="table align-middle">
-
     <thead>
         <th>Prénom</th>
         <th>Nom</th>
@@ -37,32 +36,30 @@ include '../../@/inc/header.php';
         <th>E-mail confirmé</th>
         <th>Salle</th>
         <th>Action</th>
-
-        </thead>
+    </thead>
+    <tbody>
         <?php
-        
         foreach($users as $user => $post): ?>
-            <tbody>
-                <?php echo '<th>'. $post->username .'</th>';
-                echo '<th>'. $post->surname .'</th>';
-                echo '<th>'. $post->email .'</th>';
-                echo '<th>'. $post->age .'</th>';
-                echo '<th>'. roles($post->roles).'</th>';
-                echo '<th>'. $post->confirmed_at .'</th>';
-                echo '<th>'. fetchSalleName($post->salle_id) .'</th>';
-                echo '<th >
-                    <div class="d-flex justify-content-around">';
-                    if($post->user_secret != 7){
-                        echo'<a class="btn btn-warning my-2 mx-2" href="/App/@/admin/form.php?id=' . $post->id .'&salle_id='.$post->salle_id.'">Modifier</a>';
-                        if($post->roles = 6 || $post->roles = 4){
-                            echo '<a class="btn btn-danger my-2 mx-2" href="/App/@/admin/delete.php?id=' . $post->id .'">Supprimer</a>  ';
-                        }
-                    }else{
-                        echo'<p>Aucune action</p><br><p></p>';
+        <?php echo '<th>'. $post->username .'</th>';
+            echo '<th>'. $post->surname .'</th>';
+            echo '<th>'. $post->email .'</th>';
+            echo '<th>'. $post->age .'</th>';
+            echo '<th>'. roles($post->roles).'</th>';
+            echo '<th>'. $post->confirmed_at .'</th>';
+            echo '<th>'. fetchSalleName($post->salle_id) .'</th>';
+            echo '<th >
+                <div class="d-flex justify-content-around">';
+                if($post->user_secret != 7){
+                    echo'<a class="btn btn-warning my-2 mx-2" href="/App/@/admin/form.php?id=' . $post->id .'&salle_id='.$post->salle_id.'">Modifier</a>';
+                    if($post->roles = 6 || $post->roles = 4){
+                        echo '<a class="btn btn-danger my-2 mx-2" href="/App/@/admin/delete.php?id=' . $post->id .'">Supprimer</a>  ';
                     }
-                    echo '</div>
-                </th>';
-            ?></tbody>
+                }else{
+                    echo'<p>Aucune action</p><br><p></p>';
+                }
+                echo '</div>
+            </th>';
+        ?></tbody>
         <?php endforeach;?>
     </table>
 </div>
