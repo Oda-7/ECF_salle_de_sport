57ab8b54828d2c8767815508a3193534f1b9f2bc
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-07 21:30:24 +0100

    test hello.php

--- App/@/admin/hello.php
+++ App/@/admin/hello.php
@@ -6,10 +6,10 @@ if(!isset($_SESSION['auth'])){
     header('Location: /');
 }
   
-require_once '/App/@/sys/functions.php';
-require_once '/App/@/sys/bd.php';
-require_once '/App/@/sys/roles.php';
-require_once '/App/@/sys/salles.php';
+require_once '../sys/functions.php';
+require_once '../sys/bd.php';
+require_once '../sys/roles.php';
+require_once '../sys/salles.php';
 echo 'hello';
 $req = $pdo->prepare('SELECT id, username, surname, email, age, roles,confirmed_at,salle_id,user_secret FROM users ORDER BY roles DESC');
 $req->execute();
