cfc99ce2da2269cbfb43c035c2fa4a2c422507c5
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-08 16:08:55 +0100

    test concluant ça bloque sur le require

--- App/@/admin/hello.php
+++ App/@/admin/hello.php
@@ -5,16 +5,18 @@ include '../inc/header.php';
 if(empty($_SESSION['auth'])){
     header('Location: /');
     exit();
+}else {
+    require '/App/@/sys/bd.php';
 }
 
-require_once '../sys/functions.php';
-/*require_once '/App/@/sys/bd.php';
+require '../sys/functions.php';
+
 echo 'hello';
 require_once '/App/@/sys/roles.php';
 require_once '/App/@/sys/salles.php';
 $req = $pdo->prepare('SELECT id, username, surname, email, age, roles,confirmed_at,salle_id,user_secret FROM users ORDER BY roles DESC');
 $req->execute();
-$users = $req->fetchAll();*/
+$users = $req->fetchAll();
 
 ?>
 
