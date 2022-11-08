43ff8bbbb90bffca0fed9c542a3196fc9cd912be
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-08 22:13:13 +0100

    l:55 transformer en if() admin.php

--- App/@/admin/admin.php
+++ App/@/admin/admin.php
@@ -52,7 +52,7 @@ require_once '../sys/functions.php';
             <div class="d-flex justify-content-around">';
             if($post->user_secret !== 7){
                 echo'<a class="btn btn-warning my-2 mx-2" href="/App/@/admin/form.php?id=' . $post->id .'&salle_id='.$post->salle_id.'">Modifier</a>';
-                if($post->roles == 6 || $post->roles == 4){
+                if($_SESSION['auth']->roles == 6 || $_SESSION['auth']->roles == 4 ){
                     echo '<a class="btn btn-danger my-2 mx-2" href="/App/@/admin/delete.php?id=' . $post->id .'">Supprimer</a>  ';
                 }
             }else{
