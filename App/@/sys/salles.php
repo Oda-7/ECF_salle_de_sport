eeff70bce6a901553bf128d0ea39b0ab054c2d66
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-09 20:50:09 +0100

    test var_dump() salles.php

--- App/@/sys/salles.php
+++ App/@/sys/salles.php
@@ -4,6 +4,7 @@ function fetchSalleName($salle_id)
 {
     if(empty($salle_id)){
         return 'Aucune salle lié ';
+        return var_dump($salle_id);
     }else{
         require 'bd.php';
         $req = $pdo->prepare('SELECT name FROM salles WHERE id = "'.$salle_id.'"');
