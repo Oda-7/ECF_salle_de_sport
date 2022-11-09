b71dd36109053c7975120001bc73f144b1637fdb
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-09 16:11:40 +0100

    delete l:55

--- App/@/admin/admin.php
+++ App/@/admin/admin.php
@@ -52,7 +52,7 @@ require_once '../sys/functions.php';
             <div class="d-flex justify-content-around">';
             if($post->user_secret !== 7){
                 echo'<a class="btn btn-warning my-2 mx-2" href="/App/@/admin/form.php?id=' . $post->id .'&salle_id='.$post->salle_id.'">Modifier</a>';
-                if($_SESSION['auth']->roles == 6 || $_SESSION['auth']->roles == 3 ){
+                if($_SESSION['auth']->roles == 6 ){
                     echo '<a class="btn btn-danger my-2 mx-2" href="/App/@/admin/delete.php?id=' . $post->id .'">Supprimer</a>  ';
                 }
             }else{
