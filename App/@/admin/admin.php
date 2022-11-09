7a4bed5d1702579ac141605952963edc73255ef4
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-09 20:24:49 +0100

    bon test admin.php

--- App/@/admin/admin.php
+++ App/@/admin/admin.php
@@ -35,7 +35,7 @@ require_once '../sys/functions.php';
         $req = $pdo->prepare('SELECT id, username, surname, email, age, roles,confirmed_at,salle_id,user_secret FROM users ORDER BY roles DESC');
         $req->execute();
         $users = $req->fetchAll();
-        debug(fetchSalleName($users[3]));
+        debug(fetchSalleName($users[3]->salle_id));
 
         require_once '../sys/roles.php';
         require_once '../sys/salles.php';
