315b549b2a5a249c1c39ab4e2ea445095131864c
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-09 20:38:14 +0100

    test function salles.php

--- App/@/sys/salles.php
+++ App/@/sys/salles.php
@@ -5,7 +5,7 @@ function fetchSalleName($salle_id)
     if(empty($salle_id)){
         echo 'Aucune salle lié';
     }else{
-        require 'bd.php';
+        require '/App/@/sys/bd.php';
         $req = $pdo->prepare('SELECT name FROM salles WHERE id = "'.$salle_id.'"');
         $req->execute();
         $salle_name = $req->fetch();
