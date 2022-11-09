f99bdc655e5442a666d22e206b07191492781a85
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-09 20:43:49 +0100

    delete var_dump

--- App/@/admin/admin.php
+++ App/@/admin/admin.php
@@ -35,7 +35,6 @@ require_once '../sys/functions.php';
         $req = $pdo->prepare('SELECT id, username, surname, email, age, roles,confirmed_at,salle_id,user_secret FROM users ORDER BY roles DESC');
         $req->execute();
         $users = $req->fetchAll();
-        var_dump($users[3]->salle_id);
 
         require_once '../sys/roles.php';
         require_once '../sys/salles.php';
