66ac974b2272110525e5c3f4c2cffddfe32c9a3a
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-09 21:30:24 +0100

    test salles.php

--- App/@/sys/salles.php
+++ App/@/sys/salles.php
@@ -1,13 +1,14 @@
 <?php 
+require './bd.php';
+var_dump($salle_id);
 
 function fetchSalleName($salle_id)
 {
-    if(isset($salle_id)){
-        require '/App/@/sys/bd.php';
-        var_dump($salle_id);
+    if(isset($salle_id)){  
         $req = $pdo->prepare('SELECT name FROM salles WHERE id = "'.$salle_id.'"');
         $req->execute();
         $salle_name = $req->fetch();
+        var_dump($salle_name);
         return $salle_name;
     }
     return "Aucune salle n'est relié ";
