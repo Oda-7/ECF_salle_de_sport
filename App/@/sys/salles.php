4f47418494a11f412188eedc2866e0ccfa8ceb7c
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-09 21:31:51 +0100

    test salles.php

--- App/@/sys/salles.php
+++ App/@/sys/salles.php
@@ -1,10 +1,11 @@
 <?php 
-require './bd.php';
+require 'bd.php';
 var_dump($salle_id);
 
 function fetchSalleName($salle_id)
 {
     if(isset($salle_id)){  
+        
         $req = $pdo->prepare('SELECT name FROM salles WHERE id = "'.$salle_id.'"');
         $req->execute();
         $salle_name = $req->fetch();
