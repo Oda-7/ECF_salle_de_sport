7a825b48f4772624a23db04a94c562c62efb167c
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-09 20:19:52 +0100

    debug() admin.php

--- App/@/admin/admin.php
+++ App/@/admin/admin.php
@@ -35,6 +35,7 @@ require_once '../sys/functions.php';
         $req = $pdo->prepare('SELECT id, username, surname, email, age, roles,confirmed_at,salle_id,user_secret FROM users ORDER BY roles DESC');
         $req->execute();
         $users = $req->fetchAll();
+        debug($users);
 
         require_once '../sys/roles.php';
         require_once '../sys/salles.php';
