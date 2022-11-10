68c278c73c34faf5bab5a22a55c50f33a3d0379e
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-10 15:14:49 +0100

    test

--- App/public/src/login.php
+++ App/public/src/login.php
@@ -25,9 +25,8 @@ if(isset($_POST['submit'])){
 
                     if(isset($_POST['remember'])){
                         $remember_token = str_random(250);
-                        $req_remember = $pdo->prepare('UPDATE users SET remember_token = ? WHERE id = "'. $user->id .'"');
-                        $req_remember->execute([$remember_token]);
-                        var_dump($req_rememberq);
+                        $req_remember = $pdo->prepare('UPDATE users SET remember_token = ? WHERE id = ?');
+                        $req_remember->execute([$remember_token, $user->id]);
                         setcookie('remember', $user->id . '//' . $remember_token . sha1($user->id . 'ratonlaveurs'), time() + 60 * 60 * 24 * 7);
                         echo 'la';
                     }
