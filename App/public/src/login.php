634e861f0973eb64373e5b1010b7983dd77f6cc5
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-10 15:38:45 +0100

    var_dump(->id)

--- App/public/src/login.php
+++ App/public/src/login.php
@@ -24,10 +24,10 @@ if(isset($_POST['submit'])){
                     $_SESSION['flash']['success'] = "Vous étes maintenant connecté";
 
                     if($_POST['remember']){
-                        $remember_token = str_random(10);
+                        $remember_token = array(str_random(10));
                         $req_remember = $pdo->prepare('UPDATE users SET remember_token = ? WHERE id = "'.$user->id.'"');
-                        var_dump($remember_token);
-                        $req_remember->execute(array($remember_token));
+                        var_dump($user->id);
+                        $req_remember->execute($remember_token);
                         setcookie('remember', $user->id . '//' . $remember_token . sha1($user->id . 'ratonlaveurs'), time() + 60 * 60 * 24 * 7);
                         echo 'la';
                     }
