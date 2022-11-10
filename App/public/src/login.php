1e90ee37312be613f83350a657209ba7d75c4c26
Thomas Cimino <toma.cimino@hotmail.fr>
2022-11-10 16:20:03 +0100

    test avec where

--- App/public/src/login.php
+++ App/public/src/login.php
@@ -25,8 +25,9 @@ if(isset($_POST['submit'])){
 
                     if($_POST['remember']){
                         $remember_token = str_random(10);
-                        $req_remember = $pdo->prepare('UPDATE users SET remember_token = ?');
+                        $req_remember = $pdo->prepare('UPDATE users SET remember_token = ? WHERE id = "'.$user->id.'"');
                         $req_remember->execute([$remember_token]);
+                        var_dump($remember_token);
                         var_dump($req_remember->execute());
                         setcookie('remember', $user->id . '//' . $remember_token . sha1($user->id . 'ratonlaveurs'), time() + 60 * 60 * 24 * 7);
                         echo 'la';
